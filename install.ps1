eb6f08623f1c03e5438e79cd6c1643f2c3cf36e6
Markus Fischbacher <453368+rockaut@users.noreply.github.com>
2022-01-11 09:49:05 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -7,5 +7,9 @@ Remove-Item -Path $env:TEMP\starship.zip
 
 Copy-Item -Verbose -Path $env:USERPROFILE\code\personal\dotfiles\common\* -Destination $env:USERPROFILE\
 
-Copy-Item -Verbose -Path $env:USERPROFILE\code\personal\dotfiles\win\Microsoft.PowerShell_profile.ps1 $PROFILE
+New-Item -Type Directory -Force -Path $env:USERPROFILE\Documents\PowerShell
+New-Item -Type Directory -Force -Path $env:USERPROFILE\Documents\WindowsPowerShell
+Copy-Item -Verbose -Path $env:USERPROFILE\code\personal\dotfiles\win\Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\Documents\PowerShell
+Copy-Item -Verbose -Path $env:USERPROFILE\code\personal\dotfiles\win\Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\Documents\WindowsPowerShell
+
 Copy-Item -Verbose -Path $env:USERPROFILE\code\personal\dotfiles\win\terminal.settings.json $env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
